6f86964a2cb19b37175b0013416b4b72037f08f8
Joeri <joeri@cypherpunk.nl>
2017-11-23 13:25:36 +0100

    Fixed typo in image name

--- matrixssl/matrixssl.sh
+++ matrixssl/matrixssl.sh
@@ -4,17 +4,17 @@ typeset -i i=0 max=${#array[*]}
 while (( i < max ))
 do
 	echo "Building: Matrixssl 3-${array[$i]}"
-	docker build --build-arg VERSION=${array[$i]} -t martixssl-3-${array[$i]}-server -f Dockerfile-3_x .
+	docker build --build-arg VERSION=${array[$i]} -t matrixssl-3-${array[$i]}-server -f Dockerfile-3_x .
 	i=i+1
 done
 
-docker build -t martixssl-3-8-3-server -f Dockerfile-3_8_3 .
+docker build -t matrixssl-3-8-3-server -f Dockerfile-3_8_3 .
 array=(7.2 4.0)
 typeset -i i=0 max=${#array[*]}
 while (( i < max ))
 do
 	echo "Building: Matrixssl 3-${array[$i]}"
-	docker build --build-arg VERSION=${array[$i]} -t martixssl-3-${array[$i]}-server -f Dockerfile-3_7_2_and_3_4_0 .
+	docker build --build-arg VERSION=${array[$i]} -t matrixssl-3-${array[$i]}-server -f Dockerfile-3_7_2_and_3_4_0 .
 	i=i+1
 done
 
