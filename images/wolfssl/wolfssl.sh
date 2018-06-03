ef82c91a56c71699f467a4540d1e868a2c7c9c54
jankaisr <jan.kaiser-e5q@rub.de>
2018-06-03 23:19:44 +0200

    Added wolfssl client client errors for versions above 3.7.0

--- images/wolfssl/wolfssl.sh
+++ images/wolfssl/wolfssl.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-array=(12.2-stable 12.0-stable 11.0-stable 10.4 10.3 10.2-stable 10.0a 10.0-stable 9.10b 9.10-stable 9.8 9.6w 9.6 9.1 9.0 7.0 6.9d 6.9c 6.9b 6.9 6.8 6.6 6.2 6.0b 6.0 4.8 4.6 4.2 4.0 3.3 3.2 3.0 2.6 2.4 2.0 1.0 0.2 0.0)
+array=(12.2-stable 12.0-stable 11.0-stable 10.4 10.3 10.2-stable 10.0a 10.0-stable 9.10b 9.10-stable 9.8 9.6w 9.6 9.1 9.0 7.0 6.9d 6.9c 6.9b 6.9 6.8 6.6 6.2 6.0b 6.0 4.8 4.6 4.2 4.0 3.3)
 typeset -i i=0 max=${#array[*]}
 while (( i < max ))
 do
 	echo "Building: WolfSSL 3.${array[$i]}"
-	docker build --build-arg VERSION=${array[$i]} -t wolfssl_3.${array[$i]}-server -f Dockerfile-3_x .
+	docker build --build-arg VERSION=${array[$i]} -t wolfssl_3.${array[$i]}-server -f Dockerfile-3_x --target wolfssl-server .
+	docker build --build-arg VERSION=${array[$i]} -t wolfssl_3.${array[$i]}-client -f Dockerfile-3_x --target wolfssl-client .
 	i=i+1
 done
 
@@ -13,6 +14,7 @@ typeset -i i=0 max=${#array[*]}
 while (( i < max ))
 do
 	echo "Building: WolfSSL 3.${array[$i]}"
-	docker build --build-arg VERSION=${array[$i]} -t wolfssl_3.${array[$i]}-server -f Dockerfile-3_3_2 .
+	docker build --build-arg VERSION=${array[$i]} -t wolfssl_3.${array[$i]}-server -f Dockerfile-3_3_2 --target wolfssl-server .
+	docker build --build-arg VERSION=${array[$i]} -t wolfssl_3.${array[$i]}-client -f Dockerfile-3_3_2 --target wolfssl-client .
 	i=i+1
 done
