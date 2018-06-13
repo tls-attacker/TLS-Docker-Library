132f62ca20ba29d817007938e32576792fc76452
jankaisr <jan.kaiser-e5q@rub.de>
2018-06-14 00:12:39 +0200

    added build commands for version 1.1.0

--- images/openssl/openssl-1_1_0X.sh
+++ images/openssl/openssl-1_1_0X.sh
@@ -2,7 +2,8 @@
 
 array=(a b c d e f -pre3)
 typeset -i i=0 max=${#array[*]}
-
+docker build --build-arg VERSION= -t openssl-1_1_0-server -f Dockerfile-1_1_0x --target openssl-server .
+docker build --build-arg VERSION= -t openssl-1_1_0-client -f Dockerfile-1_1_0x --target openssl-client .
 while (( i < max ))
 do
 	echo "Feld $i: Openssl 1.1.0${array[$i]}"
