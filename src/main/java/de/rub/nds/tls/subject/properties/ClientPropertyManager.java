c386ed70015f16b2c1d4cdf66357c98ce5ce27fc
jankaisr <jan.kaiser-e5q@rub.de>
2018-06-14 00:13:22 +0200

    rearranged and modified propertylists

--- src/main/java/de/rub/nds/tls/subject/properties/ClientPropertyManager.java
+++ src/main/java/de/rub/nds/tls/subject/properties/ClientPropertyManager.java
@@ -15,12 +15,12 @@ public class ClientPropertyManager {
         propertyList.add(new ClientImageProperties(TlsImplementationType.BORINGSSL, "master"));
         propertyList.add(new ClientImageProperties(TlsImplementationType.BOTAN, "2.5"));
         propertyList.add(new ClientImageProperties(TlsImplementationType.GNUTLS, "3.5.16"));
-        propertyList.add(new ClientImageProperties(TlsImplementationType.LIBRESSL, ""));
-        propertyList.add(new ClientImageProperties(TlsImplementationType.MATRIXSSL, ""));
+        propertyList.add(new ClientImageProperties(TlsImplementationType.LIBRESSL, "2.6.3"));
+        propertyList.add(new ClientImageProperties(TlsImplementationType.MATRIXSSL, "3.9.3"));
         propertyList.add(new ClientImageProperties(TlsImplementationType.MBED, "2.6.0"));
         //propertyList.add(new ClientImageProperties(TlsImplementationType.OCAML_TLS, ""));
         propertyList.add(new ClientImageProperties(TlsImplementationType.OPENSSL, "1.1.0f"));
-        propertyList.add(new ClientImageProperties(TlsImplementationType.S2N, ""));
+        propertyList.add(new ClientImageProperties(TlsImplementationType.S2N, "latest"));
         propertyList.add(new ClientImageProperties(TlsImplementationType.WOLFSSL, "3.12.2-stable"));
     }
 
--- src/main/java/de/rub/nds/tls/subject/properties/ServerPropertyManager.java
+++ src/main/java/de/rub/nds/tls/subject/properties/ServerPropertyManager.java
@@ -19,20 +19,22 @@ public class ServerPropertyManager {
 
     public ServerPropertyManager() {
         propertyList = new LinkedList<>();
-        propertyList.add(new ServerImageProperties(TlsImplementationType.MBED, 4430, "2.6.0", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.OPENSSL, 4433, "1.1.0f", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.DAMNVULNERABLEOPENSSL, 4433, "1.0", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.NSS, 4430, "", "/cert/db/", "cert"));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.GNUTLS, 5556, "3.5.16", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.BORINGSSL, 4430, "master", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.S2N, 4430, "", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.LIBRESSL, 4433, "", Const.RUST_TEST_CA_KEY, Const.RUST_TEST_CA_FULLCHAIN));
         propertyList.add(new ServerImageProperties(TlsImplementationType.BEARSSL, 4433, "0.5", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.BORINGSSL, 4430, "master", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
         propertyList.add(new ServerImageProperties(TlsImplementationType.BOTAN, 443, "2.5", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.WOLFSSL, 11111, "3.12.2-stable", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
-        propertyList.add(new ServerImageProperties(TlsImplementationType.RUSTLS, 443, "", Const.RUST_TEST_CA_KEY, Const.RUST_TEST_CA_FULLCHAIN));
         propertyList.add(new ServerImageProperties(TlsImplementationType.BOUNCYCASTLE, 4433, "1.58", "/cert/keys.jks", "/cert/keys.jks"));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.DAMNVULNERABLEOPENSSL, 4433, "1.0", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.GNUTLS, 5556, "3.5.16", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
         propertyList.add(new ServerImageProperties(TlsImplementationType.JSSE, 4433, "openjdk:8u162-jre-slim-bc-1-59", "/cert/keys.jks", "/cert/keys.jks"));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.LIBRESSL, 4433, "2.6.3", Const.RUST_TEST_CA_KEY, Const.RUST_TEST_CA_FULLCHAIN));
+        //propertyList.add(new ServerImageProperties(TlsImplementationType.MATRIXSSL, ...
+        propertyList.add(new ServerImageProperties(TlsImplementationType.MBED, 4430, "2.6.0", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.NSS, 4430, "", "/cert/db/", "cert"));
+        //propertyList.add(new ServerImageProperties(TlsImplementationType.OCAML_TLS, ...
+        propertyList.add(new ServerImageProperties(TlsImplementationType.OPENSSL, 4433, "1.1.0f", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.RUSTLS, 443, "", Const.RUST_TEST_CA_KEY, Const.RUST_TEST_CA_FULLCHAIN));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.S2N, 4430, "latest", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
+        propertyList.add(new ServerImageProperties(TlsImplementationType.WOLFSSL, 11111, "3.12.2-stable", Const.CERT_KEY_PEM, Const.CERT_CERT_PEM));
     }
 
     public ServerImageProperties getProperties(TlsImplementationType type) {
