0c7009a6f4cb78a6b6f9451a68b275df100c932c
jankaisr <jan.kaiser-e5q@rub.de>
2018-06-14 22:15:10 +0200

    building all botan versions is working correctly

--- images/botan/botan-1_11_X.sh
+++ images/botan/botan-1_11_X.sh
@@ -21,14 +21,13 @@ do
 	i=i+1
 done
 
-#Build error in test with v33 -> check that
 array=(14 15 16 17 19 20 21 22 23 24 30 31 32 33)
 typeset -i i=0 max=${#array[*]}
 while (( i < max ))
 do
 	echo "Building: Botan 1.11.${array[$i]}"
-	#docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-server -f Dockerfile-1_11_x --target botan-server .
-	#docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-client -f Dockerfile-1_11_x --target botan-client .
+	docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-server -f Dockerfile-1_11_x --target botan-server .
+	docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-client -f Dockerfile-1_11_x --target botan-client .
 	i=i+1
 done
 
