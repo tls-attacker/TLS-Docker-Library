56b8c09a3022953cea672e8b722f08e42e935622
jankaisr <jan.kaiser-e5q@rub.de>
2018-06-14 13:27:48 +0200

    fixed build of botan 1_11_x versions

--- images/botan/botan-1_11_X.sh
+++ images/botan/botan-1_11_X.sh
@@ -21,16 +21,19 @@ do
 	i=i+1
 done
 
-array=(14 15 16 17 19 20 21 22 23 24 30 31 32 33 34)
+#Build error in test with v33 -> check that
+array=(14 15 16 17 19 20 21 22 23 24 30 31 32 33)
 typeset -i i=0 max=${#array[*]}
 while (( i < max ))
 do
 	echo "Building: Botan 1.11.${array[$i]}"
-	docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-server -f Dockerfile-1_11_x --target botan-server .
-	docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-client -f Dockerfile-1_11_x --target botan-client .
+	#docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-server -f Dockerfile-1_11_x --target botan-server .
+	#docker build --build-arg VERSION=${array[$i]} -t botan-1_11_${array[$i]}-client -f Dockerfile-1_11_x --target botan-client .
 	i=i+1
 done
 
+docker build --build-arg VERSION=34 -t botan-1_11_34-server -f Dockerfile-1_11_34 --target botan-server .
+	docker build --build-arg VERSION=34 -t botan-1_11_34-client -f Dockerfile-1_11_34 --target botan-client .
 
 array=(25 26 27 28 29)
 typeset -i i=0 max=${#array[*]}
