8beab5ca144dc4eef6c7de662dd816e9f2d0cdfd
jankaisr <jan.kaiser-e5q@rub.de>
2018-07-26 14:05:27 +0200

    added working firefox clients from version 61 to 50

--- images/firefox/firefox.sh
+++ images/firefox/firefox.sh
@@ -2,4 +2,12 @@
 
 echo "Building Firefox Images"
 
-docker build --build-arg VERSION=61.0.1 -t ubuntu-firefox-61.0.1-client --target firefox-client .
+array=(61.0.1 60.0.2 59.0.3 58.0.2 57.0.4 56.0.2 55.0.3 54.0.1 53.0.3 52.0.2 51.0.1 50.0.2)
+typeset -i i=0 max=${#array[*]}
+while (( i < max ))
+do
+	echo "Building: Firefox ${array[$i]}"
+	docker build --build-arg VERSION=${array[$i]} -t firefox-${array[$i]}-client .
+	i=i+1
+done
+
