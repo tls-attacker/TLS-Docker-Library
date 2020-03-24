2291f25850f2b5908ffe86a60549f45ee71ad291
ic0ns <robert.merget@rub.de>
2020-03-24 09:17:10 +0100

    added newer openssl versions

--- images/openssl/openssl-1_1_1X.sh
+++ images/openssl/openssl-1_1_1X.sh
@@ -2,7 +2,16 @@
 cd "$(dirname "$0")" || exit 1
 source ../helper-functions.sh
 
-_docker build --build-arg VERSION="1.1.1-pre2" -t openssl-1_1_1-pre2-server -f Dockerfile-1_1_1_pre2 --target openssl-server .
-_docker build --build-arg VERSION="1.1.1-pre2" -t openssl-1_1_1-pre2-client -f Dockerfile-1_1_1_pre2 --target openssl-client .
+array=(a b c d e -pre1 -pre2 -pre3 -pre4 -pre5 -pre6 -pre7 -pre8 -pre9)
+typeset -i i=0 max=${#array[*]}
+_docker build --build-arg VERSION= -t openssl-1_1_1-server -f Dockerfile-1_1_1x --target openssl-server .
+_docker build --build-arg VERSION= -t openssl-1_1_1-client -f Dockerfile-1_1_1x --target openssl-client .
+while (( i < max ))
+do
+	echo "Feld $i: Openssl 1.1.1${array[$i]}"
+	_docker build --build-arg VERSION=${array[$i]} -t openssl-1_1_1${array[$i]}-server -f Dockerfile-1_1_1x --target openssl-server .
+	_docker build --build-arg VERSION=${array[$i]} -t openssl-1_1_1${array[$i]}-client -f Dockerfile-1_1_1x --target openssl-client .
+	i=i+1
+done
 
 exit "$EXITCODE"
