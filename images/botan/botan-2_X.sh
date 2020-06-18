07ef2869dc74c81cb8dde2374091c55074e77fc0
Philipp Nieting <philipp@nieting.de>
2020-06-19 00:15:27 +0200

    Fixed and updated Botan

--- images/botan/botan-2_X.sh
+++ images/botan/botan-2_X.sh
@@ -2,19 +2,19 @@
 cd "$(dirname "$0")" || exit 1
 source ../helper-functions.sh
 
-_docker build --build-arg VERSION=0 -t ${DOCKER_REPOSITORY}botan-server:2.0.0 -f Dockerfile-2_0_x --target botan-server .
-_docker build --build-arg VERSION=0 -t ${DOCKER_REPOSITORY}botan-client:2.0.0 -f Dockerfile-2_0_x --target botan-client .
+versions=(0.0 0.1 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0)
+for i in "${versions[@]}"
+do
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}botan-server:2.${i} -f Dockerfile-2_0-10 --target botan-server .
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}botan-client:2.${i} -f Dockerfile-2_0-10 --target botan-client .
+done
 
-_docker build --build-arg VERSION=1 -t ${DOCKER_REPOSITORY}botan-server:2.0.1 -f Dockerfile-2_0_x --target botan-server .
-_docker build --build-arg VERSION=1 -t ${DOCKER_REPOSITORY}botan-client:2.0.1 -f Dockerfile-2_0_x --target botan-client .
 
-_docker build --build-arg VERSION=0 -t ${DOCKER_REPOSITORY}botan-server:2.1.0 -f Dockerfile-2_1_x --target botan-server .
-_docker build --build-arg VERSION=0 -t ${DOCKER_REPOSITORY}botan-client:2.1.0 -f Dockerfile-2_1_x --target botan-client .
-
-_docker build --build-arg VERSION=0 -t ${DOCKER_REPOSITORY}botan-server:2.2.0 -f Dockerfile-2_2_x --target botan-server .
-_docker build --build-arg VERSION=0 -t ${DOCKER_REPOSITORY}botan-client:2.2.0 -f Dockerfile-2_2_x --target botan-client .
-
-_docker build -t ${DOCKER_REPOSITORY}botan-server:2.3.0 -f Dockerfile-2_3_0 --target botan-server .
-_docker build -t ${DOCKER_REPOSITORY}botan-client:2.3.0 -f Dockerfile-2_3_0 --target botan-client .
+versions=(11.0 12.0 12.1 13.0 14.0)
+for i in "${versions[@]}"
+do
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}botan-server:2.${i} -f Dockerfile-2_11-x --target botan-server .
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}botan-client:2.${i} -f Dockerfile-2_11-x --target botan-client .
+done
 
 exit "$EXITCODE"
