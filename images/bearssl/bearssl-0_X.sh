c2f5cbb87cae685ce1fc1aed4b890b35ba45d393
Philipp Nieting <philipp@nieting.de>
2020-06-20 15:19:34 +0200

    Updated bearssl

--- images/bearssl/bearssl-0_X.sh
+++ images/bearssl/bearssl-0_X.sh
@@ -2,10 +2,10 @@
 cd "$(dirname "$0")" || exit 1
 source ../helper-functions.sh
 
-_docker build --build-arg VERSION=4 -t ${DOCKER_REPOSITORY}bearssl_server:0.4 -f Dockerfile-0_x --target bearssl-server .
-_docker build --build-arg VERSION=4 -t ${DOCKER_REPOSITORY}bearssl_client:0.4 -f Dockerfile-0_x --target bearssl-client .
-
-_docker build --build-arg VERSION=5 -t ${DOCKER_REPOSITORY}bearssl_server:0.5 -f Dockerfile-0_x --target bearssl-server .
-_docker build --build-arg VERSION=5 -t ${DOCKER_REPOSITORY}bearssl_client:0.5 -f Dockerfile-0_x --target bearssl-client .
+versions=(0.4 0.5 0.6)
+for i in "${versions[@]}"; do
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}bearssl_server:${i} -f Dockerfile-0_x --target bearssl-server .
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}bearssl_client:${i} -f Dockerfile-0_x --target bearssl-client .
+done
 
 exit "$EXITCODE"
