466d8a6d95ef3b29ac4f37823897aaffed759c3c
Philipp Nieting <philipp@nieting.de>
2020-06-24 11:26:43 +0200

    Fixed bearssl docker tags

--- images/bearssl/bearssl-0_X.sh
+++ images/bearssl/bearssl-0_X.sh
@@ -4,8 +4,8 @@ source ../helper-functions.sh
 
 versions=(0.4 0.5 0.6)
 for i in "${versions[@]}"; do
-    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}bearssl_server:${i} -f Dockerfile-0_x --target bearssl-server .
-    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}bearssl_client:${i} -f Dockerfile-0_x --target bearssl-client .
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}bearssl-server:${i} -f Dockerfile-0_x --target bearssl-server .
+    _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}bearssl-client:${i} -f Dockerfile-0_x --target bearssl-client .
 done
 
 exit "$EXITCODE"
