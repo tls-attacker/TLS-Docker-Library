c3da1059d339db788e1f6cd850bf48022f1968a1
Philipp Nieting <philipp@nieting.de>
2020-07-01 10:44:17 +0200

    Updated s2n

--- images/s2n/s2n.sh
+++ images/s2n/s2n.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")" || exit 1
 source ../helper-functions.sh
 
 
-versions=(0.10.5 0.10.4 0.10.3 0.10.2 0.10.1 0.10.0 0.9.0)
+versions=(0.10.7 0.10.6 0.10.5 0.10.4 0.10.3 0.10.2 0.10.1 0.10.0 0.9.0)
 for i in "${versions[@]}"; do
     _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}s2n-server:${i} --target s2n-server .
     _docker build --build-arg VERSION=${i} -t ${DOCKER_REPOSITORY}s2n-client:${i} --target s2n-client .
