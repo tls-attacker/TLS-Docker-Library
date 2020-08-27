ab94eb67ff2a3ce8a00139f07494d4f7d89fe1af
Sven Niclas Hebrok <snhebrok@mail.uni-paderborn.de>
2020-08-27 15:01:42 +0200

    Modify certs setup script
    Fix script crashing when failing to remove volume
    Use docker cp instead of mounting local directories into the container

--- certs/setup.sh
+++ certs/setup.sh
@@ -4,9 +4,13 @@ set -eu
 
 docker build -t certs -f Dockerfile .
 
-docker volume remove cert-data
+docker volume remove cert-data || true
 docker volume create cert-data
 docker run --rm -v cert-data:/cert/ certs cp -r ./. /cert/
 
 echo "Copying Root CA Certificate to relevant image folders"
-docker run --rm -v cert-data:/cert/ -v $(pwd)/../images:/dst/ debian /bin/bash -c "cp /cert/ca.pem /dst/baseimage/ && cp /cert/ca.pem /dst/firefox/"
+docker run --rm -d -v cert-data:/cert/ --name tmp-certs certs sleep infinity
+docker cp tmp-certs:/cert/ca.pem "$(pwd)/../images/baseimage/"
+docker cp tmp-certs:/cert/ca.pem "$(pwd)/../images/firefox/"
+docker cp tmp-certs:/cert/keys.jks "$(pwd)/keys.jks"
+docker kill tmp-certs
