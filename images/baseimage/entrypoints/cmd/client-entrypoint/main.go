f987b742ad83752c5d159d8eefa700a4213cf5a0
Sven Niclas Hebrok <snhebrok@mail.uni-paderborn.de>
2020-08-31 14:17:54 +0200

    Modify client-entrypoint to allow sleeping when no args are passed

--- images/baseimage/entrypoints/cmd/client-entrypoint/main.go
+++ images/baseimage/entrypoints/cmd/client-entrypoint/main.go
@@ -4,20 +4,30 @@ import (
 	"entrypoints/lib"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 )
-
 
 func trigger(w http.ResponseWriter, req *http.Request) {
 	go lib.ExecuteArgs()
 	fmt.Fprintf(w, "done")
 }
 
-
 func main() {
-	go lib.ExecuteArgs()
+	if len(os.Args) > 1 {
+		go lib.ExecuteArgs()
 
-	http.HandleFunc("/trigger", trigger)
-	http.HandleFunc("/shutdown", lib.Shutdown)
-	fmt.Println("Listening on :8090...")
-	_ = http.ListenAndServe(":8090", nil)
+		http.HandleFunc("/trigger", trigger)
+		http.HandleFunc("/shutdown", lib.Shutdown)
+		fmt.Println("Listening on :8090...")
+		_ = http.ListenAndServe(":8090", nil)
+	} else {
+		fmt.Println("No args specified; sleeping forever just to keep the container alive")
+		// Using a fancier sleep (as in https://stackoverflow.com/a/36419222/) causes go to die,
+		// as all routines are sleeping, which is interpreted as a deadlock
+		// So we just sleep for an hour forever
+		for true {
+			time.Sleep(time.Hour)
+		}
+	}
 }
