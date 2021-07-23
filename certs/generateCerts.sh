76ced3f24f28f899d70555914f72908aaf6e586b
mmaehren <32199075+mmaehren@users.noreply.github.com>
2021-07-23 10:39:59 +0200

    Added warning to script as it can't be run properly outside of docker

--- certs/generateCerts.sh
+++ certs/generateCerts.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eu
 
+WARN='\033[0;31m'
+NO_COLOR='\033[0m'
+
+CFG_PATH=/run/cert.cfg
+if [ ! -f "$CFG_PATH" ]; then
+    echo "\nWARNING: Config file $CFG_PATH does not exist. This script is intended to be run by the Dockerfile.\n"
+fi
+
 mkdir ./ca ./ca/newcerts
 touch ./ca/index.txt
 echo "unique_subject = no" > ./ca/index.txt.attr
@@ -10,13 +18,13 @@ echo "Generating Root CA Certificate"
 openssl req -new -nodes -x509 -subj "/C=DE/ST=NRW/L=Bochum/O=RUB/OU=NDS/CN=NDS CA" -key ca_key.pem -out ca.pem -days 1024
 echo "Generating RSA keys"
 openssl genpkey -algorithm RSA -out rsa2048key.pem -pkeyopt rsa_keygen_bits:2048
-openssl req -new -config /run/cert.cfg -nodes -key rsa2048key.pem -out rsa2048cert.csr
-openssl ca -config /run/cert.cfg -create_serial -batch -in rsa2048cert.csr -out rsa2048cert.pem
+openssl req -new -config $CFG_PATH -nodes -key rsa2048key.pem -out rsa2048cert.csr
+openssl ca -config $CFG_PATH -create_serial -batch -in rsa2048cert.csr -out rsa2048cert.pem
 cat rsa2048key.pem rsa2048cert.pem > rsa2048combined.pem
 echo "Generating EC keys"
 openssl genpkey -algorithm EC -out ec256key.pem -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve
-openssl req -new -config /run/cert.cfg -nodes -key ec256key.pem -out ec256cert.csr
-openssl ca -config /run/cert.cfg -create_serial -batch -in ec256cert.csr -out ec256cert.pem
+openssl req -new -config $CFG_PATH -nodes -key ec256key.pem -out ec256cert.csr
+openssl ca -config $CFG_PATH -create_serial -batch -in ec256cert.csr -out ec256cert.pem
 cat ec256key.pem ec256cert.pem > ec256combined.pem
 echo "Creating DH parameters"
 openssl dhparam -out dh.pem 2048
