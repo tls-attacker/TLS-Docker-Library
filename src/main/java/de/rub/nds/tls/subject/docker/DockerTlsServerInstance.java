c81775fe38309978f60cc96aee8f41b2b6a820f4
mmaehren <32199075+mmaehren@users.noreply.github.com>
2022-12-01 14:59:55 +0100

    Fixed NPE and empty pseudo-parameters

--- src/main/java/de/rub/nds/tls/subject/docker/DockerTlsServerInstance.java
+++ src/main/java/de/rub/nds/tls/subject/docker/DockerTlsServerInstance.java
@@ -79,18 +79,16 @@ public class DockerTlsServerInstance extends DockerTlsInstance {
         } else {
             host = hostInfo.getHostname();
         }
-        exposedImplementationPort =
-                new ExposedPort(hostInfo.getPort(), hostInfo.getType().toInternetProtocol());
-        return super.prepareCreateContainerCmd(cmd)
-                .withCmd(
-                        parameterProfile.toParameters(
-                                host,
-                                hostInfo.getPort(),
-                                imageProperties,
-                                additionalParameters,
-                                parallelize,
-                                insecureConnection))
+        exposedImplementationPort = new ExposedPort(hostInfo.getPort(), hostInfo.getType().toInternetProtocol());
+        String[] additionalCmds = parameterProfile.toParameters(host, hostInfo.getPort(), imageProperties,
+            additionalParameters, parallelize, insecureConnection);
+        if (additionalCmds.length > 0 && !additionalCmds[0].isEmpty()) {
+            return super.prepareCreateContainerCmd(cmd).withCmd(additionalCmds)
                 .withExposedPorts(exposedImplementationPort);
+        } else {
+            return super.prepareCreateContainerCmd(cmd).withExposedPorts(exposedImplementationPort);
+        }
+
     }
 
     @Override
