5758b4060ad15ee7c12c9d6cdc51ffa593a11169
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-01-20 16:29:36 +0100

    Added newest 1.1.1x version of OpenSSL

--- images/openssl/openssl-1_1_1X.sh
+++ images/openssl/openssl-1_1_1X.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")" || exit 1
 source ../helper-functions.sh
 
-array=(a b c d e f g h i m -pre1 -pre2 -pre3 -pre4 -pre5 -pre6 -pre7 -pre8 -pre9)
+array=(a b c d e f g h i m s -pre1 -pre2 -pre3 -pre4 -pre5 -pre6 -pre7 -pre8 -pre9)
 typeset -i i=0 max=${#array[*]}
 _docker build --build-arg VERSION= -t ${DOCKER_REPOSITORY}openssl-server:1.1.1 -f Dockerfile-1_1_1x --target openssl-server .
 _docker build --build-arg VERSION= -t ${DOCKER_REPOSITORY}openssl-client:1.1.1 -f Dockerfile-1_1_1x --target openssl-client .
