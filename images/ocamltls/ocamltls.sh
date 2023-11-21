5c49161492a204a16e5d9525c33b1fc2534a410d
mmaehren <32199075+mmaehren@users.noreply.github.com>
2023-11-21 08:56:19 +0100

    Cherrypick Ocaml Changes (#95)
    * Support more than one ocamltls version
    
    * Further ocamltls fixes
    
    * Even more fixes
    
    * Try installing external dependencies as well
    
    * Try another dependency approach
    
    * Try yet another dependency approach
    
    * Instead of building from scratch, use a precompiled version
    
    * Fix edit error
    
    * Install m4 before opam
    
    * libgmp changes so file name
    
    * Try with regular libgmp
    
    * Add debugging output
    
    * Try getting libgmp from /usr/lib
    
    * hybrid approach
    
    * Fix typo
    
    * explicitly install pkg-config
    
    * Use new build script
    
    * Use topkg build command
    
    * Add topkg care as a dependency
    
    * Trying another building approach
    
    * Trying another building approach
    
    * Remove leftover sed call
    
    * Running dune properly
    
    * Changing hardcoded certificate path
    
    * Removing default certificate files
    
    * Try the echo_server instead of the test_server
    
    * Try the fuzz_server instead of the test_server
    
    ---------
    
    Co-authored-by: Jan Drees <jan.drees@uni-wuppertal.de>

--- images/ocamltls/ocamltls.sh
+++ images/ocamltls/ocamltls.sh
@@ -2,7 +2,24 @@
 cd "$(dirname "$0")" || exit 1
 source ../helper-functions.sh
 
-_docker build -t ${DOCKER_REPOSITORY}ocamltls-server:0.8.0 --target ocamltls-server .
-_docker build -t ${DOCKER_REPOSITORY}ocamltls-client:0.8.0 --target ocamltls-client .
+array=(0.12.8 0.10.5)
+typeset -i i=0 max=${#array[*]}
+while (( i < max ))
+do
+	echo "Build OCaml-TLS ${array[$i]}:"
+	_docker build --build-arg VERSION=v${array[$i]} -t ${DOCKER_REPOSITORY}ocamltls-server:${array[$i]} -f Dockerfile --target ocamltls-server .
+	_docker build --build-arg VERSION=v${array[$i]} -t ${DOCKER_REPOSITORY}ocamltls-client:${array[$i]} -f Dockerfile --target ocamltls-client .
+	i=i+1
+done
+
+array=(0.10.4 0.8.0)
+typeset -i i=0 max=${#array[*]}
+while (( i < max ))
+do
+	echo "Build OCaml-TLS ${array[$i]}:"
+	_docker build --build-arg VERSION=${array[$i]} -t ${DOCKER_REPOSITORY}ocamltls-server:${array[$i]} -f Dockerfile --target ocamltls-server .
+	_docker build --build-arg VERSION=${array[$i]} -t ${DOCKER_REPOSITORY}ocamltls-client:${array[$i]} -f Dockerfile --target ocamltls-client .
+	i=i+1
+done
 
 exit "$EXITCODE"
